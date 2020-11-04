44e060b25fd4a60e1e991db896ea4740de2a1529
Ilya_Krav <Kravchenkoilya24@gmail.com>
2020-11-04 21:40:47 +0300

    init

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+using SimpleJSON;
+using System.Net;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject _panelBlock;
+    [SerializeField] private GameObject _panelBlockText;
+    [SerializeField] protected Text _text;
+
+    [SerializeField] private Text _textAll;
+    [SerializeField] private Text _textMessage;
+    [SerializeField] private Text _textError;
+
+    [SerializeField] private SortController _s;
+
+    private JSONNode _json;
+    private GameObject _activePanel;
+
+    private const float _period = 0.5f;
+    private const string _url = "http://tc-monitor.tiram.icu:5000/?key=";
+    private const string _key = "Voo0oHaj";
+
+    void Awake()
+    {
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            _panelBlock.SetActive(true);
+            _panelBlockText.SetActive(true);
+
+            return;
+        }
+
+        var webClient = new WebClient();
+        var response = webClient.DownloadString(_url + _key);
+        _json = JSONNode.Parse(response);
+
+        _textAll.text = _json.ToString();
+        _textMessage.text = _json["message"];
+        _textError.text = _json["error"];
+
+        SortController.Notify?.Invoke();
+    }
+
+    public void ButtonOpen(GameObject window)
+    {
+        if (_panelBlock.activeSelf)
+        {
+            return;
+        }
+
+        _panelBlock.SetActive(true);
+        _activePanel = window;
+
+        StartCoroutine(AnimPanel(_activePanel.transform, Vector3.one));
+    }
+
+    public void ButtonClose()
+    {
+        _panelBlock.SetActive(false);
+        StartCoroutine(AnimPanel(_activePanel.transform, Vector3.zero));
+
+        _activePanel = null;
+    }
+
+    IEnumerator AnimPanel(Transform window, Vector3 doneVector)
+    {
+        var time = 0f;
+        var scale = window.localScale;
+
+        while (time < _period)
+        {
+            time += Time.deltaTime;
+            var nTime = time / _period;
+            var lValue = Vector3.Lerp(scale, doneVector, nTime);
+            window.localScale = lValue;
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/SortController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SortController : MonoBehaviour
+{
+    [Header("Sort")]
+    [SerializeField] protected Text _text;
+    [SerializeField] protected Image _textImage;
+
+    [Header("Find by name")]
+    [SerializeField] private InputField _inputText;
+
+    private string _content;
+    private string[] _saveContentSort;
+    private string[] _contentSort;
+    private string[] _contentArray;
+    private bool _isSort;
+
+    public static Action Notify;
+
+    private void Awake()
+    {
+        Notify += SaveContent;
+    }
+
+    private void OnDestroy()
+    {
+        if (Notify != null) Notify -= SaveContent;
+    }
+
+    private void SaveContent()
+    {
+        var currentContent = _text.text;
+        _content = currentContent;
+        _contentArray = currentContent.Split(' ');
+    }
+
+    private void PripareToSort()
+    {
+        var currentContent = _text.text;
+        _contentSort = currentContent.Split(' ');
+        _saveContentSort = currentContent.Split(' ');
+    }
+
+    public void ButtonSortClick()
+    {
+        if (_isSort)
+        {
+            SetUnsort();
+        }
+        else
+        {
+           SetSort();
+        }
+    }
+
+    private void SetUnsort()
+    {
+        _text.text = string.Join(" ", _saveContentSort);
+        _textImage.color = Color.white;
+
+        _isSort = false;
+    }
+
+    private void SetSort()
+    {
+        PripareToSort();
+
+        Array.Sort(_contentSort);
+        _text.text = string.Join(" ", _contentSort);
+        _textImage.color = new Color(0.78f, 0.78f, 0.78f);
+
+        _isSort = true;
+    }
+
+    public void ButtonFindByWord()
+    {
+        if (_content == null) SaveContent();
+
+        if (_isSort)
+        {
+            SetUnsort();
+        }
+
+        var findWord = _inputText.text;
+
+        if (findWord == "")
+        {
+            _text.text = _content;
+
+            return;
+        }
+
+        var outputArray = new List<string>();
+
+        for (int i = 0; i < _contentArray.Length; i++)
+        {
+            if (_contentArray[i].Contains(findWord))
+            {
+                outputArray.Add(_contentArray[i]);
+            }
+        }
+
+        _text.text = string.Join(" ", outputArray);
+
+        PripareToSort();
+    }
+
+    public void ButtonReset()
+    {
+        if (_isSort)
+        {
+            SetUnsort();
+        }
+
+        _inputText.text = null;
+
+        if (_content == null) SaveContent();
+
+        _text.text = _content;
+    }
+}
